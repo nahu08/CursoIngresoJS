b19e6eeca82825c7d3daa1abf564502673b4821c
nahu08 <gnahu.08@gmail.com>
2017-06-29 03:04:55 +0200

    commit 2
    asdsadsad

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,15 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+var dato=0;
+//dato=document.getElementById('elNombre').value;
+//alert(dato);
+document.getElementById('nata').value="natalia natalia";
+	
+
+
+
+}
+
+
---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,12 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'MOSTRAR'
+*/
+function Mostar()
+{
+var nombre = prompt("Por favor ingresa tu nobmre", "Natalia Natalia");
+
+document.getElementById('elNombre').value="natalia natalia";
+
+}
+
